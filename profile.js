603776997a5b7ae47aef28609a6814f7f4c55600
NattyNgrn <natnrgn@gmail.com>
2023-08-03 16:57:08 -0400

    adding columns

---
+++ profile.js
@@ -0,0 +1,8 @@
+let para = document.createElement("p");
+let text1 = document.createTextNode("MEOWMEOWMOEW");
+para.appendChild(text1);
+let abme = document.getElementById("abme");
+abme.appendChild(para);
+
+
+
