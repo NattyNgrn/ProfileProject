bf80f7c450c03e5701d4916238d50be768849179
NattyNgrn <natnrgn@gmail.com>
2023-08-03 20:51:01 -0400

    chnaged layout

--- profile.js
+++ profile.js
@@ -1,8 +1,4 @@
-let para = document.createElement("p");
-let text1 = document.createTextNode("MEOWMEOWMOEW");
-para.appendChild(text1);
-let abme = document.getElementById("abme");
-abme.appendChild(para);
+
 
 
 
